b396135f57ca3cc061c53e855735348ea00bc7b5
danieele15 <144240595+danieele15@users.noreply.github.com>
2025-06-27 12:21:15 -0300

    inter

--- script.js
+++ script.js
@@ -15,26 +15,7 @@ const perguntas = [
             {
                 texto: "Isso é maravilhoso!",
                 afirmacao: "Quis saber como usar IA no seu dia a dia."
-          <!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <title>Você decide o futuro da IA</title>
-</head>
-<body>
-    <div class="caixa-principal">
-        <h1>Você decide o futuro da IA</h1>
-        <div class="caixa-perguntas"></div>
-        <div class="caixa-alternativas"></div>
-        <div class="caixa-resultado">
-            <p class="texto-resultado"></p>
-        </div>
-    </div>
-    <script src="script.js"></script>
-</body>
-</html>  }
+            }
         ]
     },
     {
